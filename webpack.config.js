1ad10107b1aa978b915728a1c2b625f3d329cc0d
Cherubin <cherubinkubwayo@gmail.com>
2020-11-10 14:06:48 +0200

    (ch-react-setup): remove title in index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-<<<<<<< HEAD
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-=======
->>>>>>> 22ece69a962e688c26562fe87ba10ece5cb854f6
 const path = require('path');
 
 module.exports = {
@@ -12,10 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-<<<<<<< HEAD
     new CleanWebpackPlugin(),
-=======
->>>>>>> 22ece69a962e688c26562fe87ba10ece5cb854f6
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
