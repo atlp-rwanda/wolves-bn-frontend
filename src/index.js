0d565990fa164f6c3bef67efb9ab905b118e7a40
Minega Shyaka Patrick <minega25@gmail.com>
2020-11-16 12:13:35 +0200

    Exclude unnecessary files in codeclimate check

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux';
 import App from './App';
-
-import store from './redux/store';
 
 ReactDOM.render(
     <App />, document.getElementById('root'),
