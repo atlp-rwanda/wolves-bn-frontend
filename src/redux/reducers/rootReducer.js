e79551b94bb2d56d9e7877565808855dbbc07f7b
Thom-IT <65611812+Thom-IT@users.noreply.github.com>
2020-11-30 09:08:41 +0200

    fix conflicts

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,15 +1,16 @@
 import { combineReducers } from 'redux';
+import roleReducer from './user.role';
 import requestsReducer from './requests/requests';
 import loginReducer from './login.reducer';
 import registerReducer from './register';
 import passwordReducer from './password.reducer';
 
 const rootReducer = combineReducers({
-
   login: loginReducer,
   reset: passwordReducer,
   requests: requestsReducer,
   signUp: registerReducer,
+  setRole: roleReducer
 
 });
 
